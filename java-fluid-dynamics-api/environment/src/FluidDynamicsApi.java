59c6c7cbecb1fd775a08315ba647996ba0c7f8e5
Matthew Penner <mtepenner@gmail.com>
2026-04-27 09:35:25 -0700

    Create FluidDynamicsApi.java

---
+++ java-fluid-dynamics-api/environment/src/FluidDynamicsApi.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class FluidDynamicsApi {
+    public static void main(String[] args) throws Exception {
+        // TODO: Bind to 0.0.0.0:8080 
+        // TODO: Implement the /simulate, /latest-report, and /health endpoints
+    }
+}
