7fbea1c7ba3049a413532fbfed239269752dc0a5
Matthew Penner <mtepenner@gmail.com>
2026-04-27 09:35:44 -0700

    Create postprocess.py

---
+++ java-fluid-dynamics-api/environment/src/postprocess.py
@@ -0,0 +1,11 @@
+import sys
+import json
+
+def main():
+    # TODO: Read raw simulation JSON from the file path passed as a CLI argument
+    # TODO: Compute mean_velocity, velocity_stddev, pressure_drop, and dominant_regime
+    # TODO: Write to latest_analysis.json and print to stdout
+    pass
+
+if __name__ == "__main__":
+    main()
