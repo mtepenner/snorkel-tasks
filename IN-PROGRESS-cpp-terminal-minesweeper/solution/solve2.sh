8af54c7ffe2d82eff36cb718c8509fdacc926fed
Matthew Penner <mtepenner@gmail.com>
2026-05-19 09:12:49 -0700

    Update solve2.sh

--- IN-PROGRESS-cpp-terminal-minesweeper/solution/solve2.sh
+++ IN-PROGRESS-cpp-terminal-minesweeper/solution/solve2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-cp solution/client.cpp /app/workspace/src/client.cpp
-
+cp /solution/client.cpp /app/workspace/src/client.cpp
 g++ /app/workspace/src/client.cpp -o /app/workspace/client -lcurl `Magick++-config --cxxflags --libs`
-echo -e "reveal 0 0\nquit" | /app/workspace/client
+echo -e "reveal 0 0\nreveal 1 1\nquit" | /app/workspace/client
